23dff736569d677cc9c8ac06554fdb1fd1a15237
dipson <dipson94@gmail.com>
2023-05-01 19:51:03 +0200

    bug fix

--- fn.py
+++ fn.py
@@ -1,5 +1,17 @@
 import numpy as np
 import pandas as pd
+"""
+only useful idenfiers from decoded .tra file
+* FLAGS corresponds to time in seconds, which makes sense to the module and object event
+* ETT+ corresponds to modules number in the Dosimis model
+* BST+ corresponds to the current event.
+* B+ and B- corresponds to entry and exit of object respectively
+* OID- corresponds to object
+identifiers from data.txt file
+* time corresponds to object
+* COM corresponds to arrival time
+
+"""
 
 def dataextraction(case,mod,path1,path2):
   
